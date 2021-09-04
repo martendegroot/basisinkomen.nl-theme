cbc12aa631b2ae03485b6cec690209911248af0f
Marten <marten.dr.degroot@gmail.com>
2021-09-04 21:47:05 +0200

    Added colored blocks

--- twentysixteen-child/template-parts/componenten/pluspunten/index.php
+++ twentysixteen-child/template-parts/componenten/pluspunten/index.php
@@ -5,23 +5,26 @@
 <div class="dranger c_arguments_favor c_component" id="arguments_favor">
   <div class="entry-content rij kolom1 rkop">
 
-    <div class="arguments">
-      <div class="arguments__header">
-        <svg class="arguments__header__icon" width="100" height="75" viewBox="0 0 94 94" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect y="34" width="94" height="26" fill="#2BC213"/>
-          <rect x="34" y="94" width="94" height="27" transform="rotate(-90 34 94)" fill="#2BC213"/>
-        </svg>
-        <h3 class="arguments__header__title">Pluspunten</h3>
-      </div>
-      <?php foreach($arguments as $argument): ?>
-        <div class="arguments__item">
-          <div class="arguments__item__icon"><?php echo $argument['icon'] ?></div>
-          <div class="arguments__item__text">
-            <h4><?php echo $argument['title'] ?></h4>
-            <p><?php echo $argument['text'] ?></p>
-          </div>
+    <div>
+      <div class="colored_spacer"></div>
+      <div class="arguments">
+        <div class="arguments__header">
+          <svg class="arguments__header__icon" width="100" height="75" viewBox="0 0 94 94" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect y="34" width="94" height="26" fill="#2BC213"/>
+            <rect x="34" y="94" width="94" height="27" transform="rotate(-90 34 94)" fill="#2BC213"/>
+          </svg>
+          <h3 class="arguments__header__title">Pluspunten</h3>
         </div>
-      <?php endforeach; ?>
+        <?php foreach($arguments as $argument): ?>
+          <div class="arguments__item">
+            <div class="arguments__item__icon"><?php echo $argument['icon'] ?></div>
+            <div class="arguments__item__text">
+              <h4><?php echo $argument['title'] ?></h4>
+              <p><?php echo $argument['text'] ?></p>
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
     </div>
 
     <?php 
--- twentysixteen-child/template-parts/componenten/tegenwerpingen/index.php
+++ twentysixteen-child/template-parts/componenten/tegenwerpingen/index.php
@@ -5,24 +5,27 @@
 <div class="dranger c_arguments_against c_component" id="arguments_against">
   <div class="entry-content kolom1 rkop">
 
-    <div class="arguments">
-      <div class="arguments__header">
-      <svg class="arguments__header__icon" width="100" height="75" viewBox="60 0 109 109" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M128 101L128 7L155 7L155 101L128 101Z" fill="#D90A0A"/>
-          <path d="M148 54.5L74.5 101.698L74.5 7.30161L148 54.5Z" fill="#D90A0A"/>
-          <path d="M98 54.5L24.5 101.698L24.5 7.30161L98 54.5Z" fill="white"/>
-        </svg>
-        <h3 class="arguments__header__title">Tegenwerpingen</h3>
-      </div>
-      <?php foreach($arguments as $argument): ?>
-        <div class="arguments__item">
-          <div class="arguments__item__icon"><?php echo $argument['icon'] ?></div>
-          <div class="arguments__item__text">
-            <h4><?php echo $argument['title'] ?></h4>
-            <p><?php echo $argument['text'] ?></p>
-          </div>
+    <div class="wrapper">
+      <div class="colored_spacer"></div>
+      <div class="arguments">
+        <div class="arguments__header">
+        <svg class="arguments__header__icon" width="100" height="75" viewBox="60 0 109 109" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M128 101L128 7L155 7L155 101L128 101Z" fill="#D90A0A"/>
+            <path d="M148 54.5L74.5 101.698L74.5 7.30161L148 54.5Z" fill="#D90A0A"/>
+            <path d="M98 54.5L24.5 101.698L24.5 7.30161L98 54.5Z" fill="white"/>
+          </svg>
+          <h3 class="arguments__header__title">Tegenwerpingen</h3>
         </div>
-      <?php endforeach; ?>
+        <?php foreach($arguments as $argument): ?>
+          <div class="arguments__item">
+            <div class="arguments__item__icon"><?php echo $argument['icon'] ?></div>
+            <div class="arguments__item__text">
+              <h4><?php echo $argument['title'] ?></h4>
+              <p><?php echo $argument['text'] ?></p>
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
     </div>
 
     <?php 
